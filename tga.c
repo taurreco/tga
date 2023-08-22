62461e527aa190566867183d071cab253aaae613
Tomas Dougan <tomasdougan@gmail.com>
2023-08-22 01:49:56 -0400

    re arranged

--- tga.c
+++ tga.c
@@ -4,9 +4,9 @@
 
 #include "tga.h"
 
-#define MAPPED          1
+#define CMAP            1
 #define RGB             2
-#define RLE_MAPPED      9
+#define RLE_CMAP        9
 #define RLE_RGB         10
 
 /*********************************************************************
@@ -22,32 +22,19 @@
 /* holds the header of a tga in memory */
 
 struct header {
-   uint8_t id_len;
-   uint8_t color_type;
-   uint8_t data_type;
-   int16_t color_start;
-   int16_t color_len;
-   uint8_t color_depth;
-   int16_t x;
-   int16_t y;
-   int16_t w;
-   int16_t h;
-   uint8_t pixel_depth;
-   uint8_t desc;
+   uint8_t idlen;
+   uint8_t cmap_type;
+   uint8_t img_type;
+   int16_t cmap_start;
+   int16_t cmap_len;
+   uint8_t cmap_depth;
+   int16_t img_x;
+   int16_t img_y;
+   int16_t img_w;
+   int16_t img_h;
+   uint8_t img_depth;
+   uint8_t img_desc;
 };
-
-/*******
- * tga *
- *******/
-
-/* represents the tga file in memory */
-
-struct tga {
-   struct header header;
-   uint8_t* colors;
-   uint8_t* pixels;
-};
-
 
 /*********************************************************************
  *                                                                   *
@@ -55,34 +42,24 @@ struct tga {
  *                                                                   *
  *********************************************************************/
 
-/**********
- * readu8 *
- **********/
+/******
+ * u8 *
+ ******/
 
 void
-readu8(uint8_t* dst, FILE* fp)
+u8(uint8_t* dst, FILE* fp)
 {
     fread(dst, 1, 1, fp);
 }
 
-/***********
- * readu16 *
- ***********/
+/*******
+ * u16 *
+ *******/
 
 void
-readu16(uint16_t* dst, FILE* fp)
+u16(uint16_t* dst, FILE* fp)
 {
-    fread(dst, 1, 2, fp);
-}
-
-/***********
- * readbuf *
- ***********/
-
-void
-readbuf(void* dst, int len, FILE* fp)
-{
-    fread(dst, 1, len, fp);
+    fread(dst, 2, 1, fp);
 }
 
 /**********
@@ -101,47 +78,37 @@ unpack(uint8_t* bytes, int n_bytes)
     g = 0;
     b = 0;
 
-    switch (n_bytes) {
+    if (n_bytes == 2) {
+        /* grab 5-bit representation of each color */
+         
+	 r = (bytes[1] >> 2) & 0x1F;
+         g = ((bytes[1] << 3) & 0x18) | ((bytes[0] >> 5) & 0x07);
+         b = (bytes[0]) & 0x1F;
+
+         /* scale channels by 8.2258 */
+         
+	 r = (r << 3) | (r >> 2);
+         g = (g << 3) | (g >> 2);
+         b = (b << 3) | (b >> 2);
+
+         /* attribute channel */
+         
+	 a = 255 * ((bytes[0] & 0x80) >> 7);
+    }
+
+    if (n_bytes == 3) {   
+        a = 255;
+        r = bytes[2];
+        g = bytes[1];
+        b = bytes[0];
+    }
         
-	case 2:
-            /* ARRRRRGG GGGBBBBB */
-            
-	    /* grab 5-bit representation of each color */
-         
-	    r = (bytes[1] >> 2) & 0x1F;
-            g = ((bytes[1] << 3) & 0x18) | ((bytes[0] >> 5) & 0x07);
-            b = (bytes[0]) & 0x1F;
-
-            /* scale channels by 8.2258 */
-         
-	    r = (r << 3) | (r >> 2);
-            g = (g << 3) | (g >> 2);
-            b = (b << 3) | (b >> 2);
-
-            /* attribute channel */
-         
-	    a = 255 * ((bytes[0] & 0x80) >> 7);
-           
-	    break;
-
-        case 3:   
-	    /* RRRRRRRR GGGGGGGG BBBBBBBB */
-
-            a = 255;
-            r = bytes[2];
-            g = bytes[1];
-            b = bytes[0];
-            break;
-        
-	case 4:  
-            /* AAAAAAAA RRRRRRRR GGGGGGGG BBBBBBBB */
-            
-	    a = bytes[3];
-            r = bytes[2];
-            g = bytes[1];
-            b = bytes[0];
-            break;
-   }
+    if (n_bytes == 4) {        
+        a = bytes[3];
+        r = bytes[2];
+        g = bytes[1];
+        b = bytes[0];
+     }
 
    return a << 24 | r << 16 | g << 8 | b;
 }
@@ -151,17 +118,10 @@ unpack(uint8_t* bytes, int n_bytes)
  **********/
 
 void
-mapped(uint32_t* data, struct tga* tga)
+mapped(struct header* hdr, uint32_t* data, uint8_t* cmap, uint8_t* img)
 {
-    struct header* header;
-    uint8_t* bytes;
-    int n_bytes;
-    
-    header = &tga->header;
-    n_bytes = header->color_depth >> 3;
-
-    for (int i = 0; i < header.w * header.h; i++) {
-        bytes = tga.colors + tga.pixels[i] * n_bytes;
+    for (int i = 0; i < header->img_w * header->img_h; i++) {
+        bytes = cmap + img[i] * n_bytes;
         data[i] = unpack(bytes, n_bytes);
     }
 }
@@ -171,17 +131,15 @@ mapped(uint32_t* data, struct tga* tga)
  *******/
 
 void
-rgb(uint32_t* data, struct tga* tga)
+rgb(struct header* hdr, uint32_t* data, uint8_t* img)
 {
-    struct header* header;
     uint8_t* bytes;
     int n_bytes;
     
-    header = &tga->header;
-    bytes = tga->pixels;
-    n_bytes = header->pixel_depth >> 3;
+    bytes = img;
+    n_bytes = hdr->img_depth >> 3;
     
-    for (int i = 0; i < header.w * header.h; i++) {
+    for (int i = 0; i < hdr->img_w * hdr->img_h; i++) {
         data[i] = unpack(bytes, n_bytes);
         bytes += n_bytes;
     }
@@ -192,7 +150,7 @@ rgb(uint32_t* data, struct tga* tga)
  *******/
 
 void
-rle(uint32_t* data, struct tga* tga, uint8_t* bytes, int stride)
+rle(struct header* hdr, uint32_t* data, uint8_t* cmap, uint8_t* img)
 {
     int pixel_bytes;
     uint8_t* packet;
@@ -230,86 +188,15 @@ rle(uint32_t* data, struct tga* tga, uint8_t* bytes, int stride)
     }
 }
 
-/********
- * read *
- ********/
-
-/* reads tga data into RAM for quick parsing */
-
-void
-read(struct tga* tga, FILE* fp)
-{
-    struct header* header;
-    uint8_t *colors, *pixels;
-    int pixel_bytes, color_bytes;
-    
-    /* fill header */
-    
-    readu8(&header->id_len, fp);
-    readu8(&header->color_type, fp);
-    readu8(&header->data_type, fp);
-    readu16(&header->color_origin, fp);
-    readu16(&header->color_len, fp);
-    readu8(&header->color_depth, fp);
-    readu16(&header->x, fp);
-    readu16(&header->y, fp);
-    readu16(&header->w, fp);
-    readu16(&header->h, fp);
-    readu8(&header->pixel_depth, fp);
-    readu8(&header->desc, fp);
-
-    pixel_bytes = header->pixel_depth / 8;
-    color_bytes = header->color_depth / 8;
-
-    /* fill color map and image data */
-
-    colors = calloc(header->color_len * color_bytes, 1);
-    pixels = calloc(header->w * header->h * pixel_bytes, 1);
-
-    fseek(fp, header->id_len, SEEK_CUR);
-    readbuf(colors, header->color_len * color_bytes, fp);
-    readbuf(pixels, header->w * header->h * pixel_bytes, fp);
-
-    tga->header = *header;
-    tga->colors = colors;
-    tga->pixels = pixels;
-}
-
-/*********************************************************************
- *                                                                   *
- *                         public definition                         *
- *                                                                   *
- *********************************************************************/
-
-/************
- * tga_load *
- ************/
+/*********
+ * parse *
+ *********/
 
 /* placeholder */
 
 int
-tga_load(char* file, uint32_t** data_p, int* width_p, int* height_p)
+parse(struct header* ihdr, uint32_t* data, uint8_t* cmap, uint8_t* img)
 {
-    FILE* fp;
-    struct tga tga;
-    struct header header;
-    uint32_t* data;
-    uint8_t* bytes;
-    int stride;
-
-    /* read file into memory */
-
-    fp = fopen(file, "rb");
-    read(&tga, fp);
-    fclose(fp);
-
-    header = tga.header;
-
-    pixel_depth = header.pixel_depth / 8;
-    color_depth = header.color_depth / 8;
-
-    data = calloc(header.w * header.h, 4);
-   
     stride = color_bytes;
     bytes = tga.colors + packet[1];
 
@@ -333,10 +220,65 @@ tga_load(char* file, uint32_t** data_p, int* width_p, int* height_p)
             free(colors);
             return 0;
     }
-
-    /* return */
-    texture->height = tga.header.height;
-    free(tga.color_map);
-    free(tga.image_data);
     return 1;
 }
+
+/*********************************************************************
+ *                                                                   *
+ *                         public definition                         *
+ *                                                                   *
+ *********************************************************************/
+
+/************
+ * tga_load *
+ ************/
+
+/* placeholder */
+
+int
+tga_load(char* file, uint32_t** data_p, int* w_p, int* h_p)
+{
+    FILE* fp;
+    struct header ihdr;
+    uint8_t *cmap, *img;
+    int size, cmap_bytes, img_bytes;
+
+    fp = fopen(fp, "r");
+
+    /* fill header */
+
+    u8(&ihdr->idlen, fp);
+    u8(&ihdr->cmap_type, fp);
+    u8(&ihdr->img_type, fp);
+    u16(&ihdr->cmap_start, fp);
+    u16(&ihdr->cmap_len, fp);
+    u8(&ihdr->cmap_depth, fp);
+    u16(&ihdr->img_x, fp);
+    u16(&ihdr->img_y, fp);
+    u16(&ihdr->img_w, fp);
+    u16(&ihdr->img_h, fp);
+    u8(&ihdr->img_depth, fp);
+    u8(&ihdr->img_desc, fp);
+
+    cmap_bytes = ihdr.cmap_depth >> 3;
+    img_bytes = ihdr.img_depth >> 3;
+
+    /* fill color map and image data */
+
+    colors = calloc(ihdr.cmap_len * cmap_bytes, 1);
+    pixels = calloc(ihdr.img_w * ihdr.img_h * img_bytes, 1);
+
+    fseek(fp, ihdr.idlen, SEEK_CUR);
+    fread(cmap, ihdr.cmap_len * cmap_bytes, 1, fp);
+    fread(img, ihdr.img_w * ihdr.img_h * img_bytes, 1, fp);
+   
+    fclose(fp);
+
+    parse(&ihdr, *data_p, cmap, img);
+    *w_p = ihdr.img_w;
+    *h_p = ihdr.img_h;
+
+    free(cmap);
+    free(img)
+}
+
